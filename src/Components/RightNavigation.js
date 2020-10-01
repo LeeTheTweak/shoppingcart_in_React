2faf1c7b8af0dc55f7fef0d834448d07de6b08b8
Christopher Leveston <chrisleveston@gmail.com>
2020-09-30 20:12:44 -0500

    Finished a click function for the list div. I have to fix a few bugs with the sizing of the list size so it won't go out of bounds.

--- src/Components/RightNavigation.js
+++ src/Components/RightNavigation.js
@@ -5,9 +5,22 @@ export default class RightNavigation extends Component {
         super();
         this.state = {
             wishList: [ ],
-            listNumber: 0
+            listNumber: 0,
+            wishListClass: 'lists-box'
         }
         this.addToWishList = this.addToWishList.bind(this);
+    }
+
+    showListToggle = () => {
+        if(this.state.wishListClass == 'lists-box') {
+            this.setState({
+                wishListClass: 'show-list-box'
+            });
+        } else if(this.state.wishListClass == 'show-list-box') {
+            this.setState({
+                wishListClass: 'lists-box'
+            });
+        }
     }
 
 
@@ -17,14 +30,13 @@ export default class RightNavigation extends Component {
 
         let addListBoxValue =  addListBox.value;
 
-        let joined = this.state.wishList.concat(addListBoxValue)
+        let joined = this.state.wishList.concat(addListBoxValue);
         
 
         this.setState({
-            wishList: joined,
-            listNumber: this.state.wishList.length + 1
+            wishList: joined
         }, () => {
-            
+            this.state.listNumber = this.state.wishList.length + 1;
         });
     }
 
@@ -46,15 +58,19 @@ export default class RightNavigation extends Component {
                 </section>
 
                 <section className="lists-section">
-                    <p>Lists <span><i class="fas fa-sort-down"></i></span></p>
-                    <div className="lists-box">
+
+                    <p onClick={this.showListToggle}>Lists <span><i class="fas fa-sort-down"></i></span></p>
+
+                    <div className={this.state.wishListClass}>
                         <h3>Your Wish List <span>{this.state.listNumber}</span></h3>
                         <div className="list">
                             {this.state.wishList}
                         </div>
+
                         <div className="addListBox-area">
                             <input className="addListBox" type="text" name="Add_List_Box" placeholder="Enter wish list name"/>
                         </div>
+
                         <button onClick={this.addToWishList}>Add to Wish List</button>
                     </div>
                 </section>
